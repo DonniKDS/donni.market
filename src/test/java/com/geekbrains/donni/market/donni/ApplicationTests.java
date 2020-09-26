0580112c7aea5fb763f5192401640a19ab7222c0
Donni <donnikds@gmail.com>
2020-09-26 11:56:50 +0300

    Initial commit

---
+++ src/test/java/com/geekbrains/donni/market/donni/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.donni.market.donni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
